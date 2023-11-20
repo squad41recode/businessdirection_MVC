159dcd7bcf2094ca4287b2e56cdb496f7680006f
Cristiele <122491782+svhellen@users.noreply.github.com>
2023-11-20 20:00:29 -0300

    Update EmpreendedorMentoria.java

--- BusinessDirection/src/main/java/br/com/BusinessDirection/model/EmpreendedorMentoria.java
+++ BusinessDirection/src/main/java/br/com/BusinessDirection/model/EmpreendedorMentoria.java
@@ -9,8 +9,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
